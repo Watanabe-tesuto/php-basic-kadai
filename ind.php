1794d9772dc13ad4883aa5a557a3c52a4344626b
Watanabe-tesuto <nekomo2015@gmail.com>
2024-12-10 22:31:47 +0900

    課題０００のphpファイルをアップロードする

--- ind.php
+++ ind.php
@@ -41,7 +41,7 @@ tax = 2 は通常税率（10%）
 利用するのは
 ・繰り返し処理
 ・条件分岐
-・関数（税込価格の計算）*/
+・関数（税込価格の計算）
 
 $price ='price'; 
 
@@ -74,8 +74,7 @@ echo   "は"  .  $price2 . "円です。<br>";
 
 /*function taxInn($tax01,$tax02){ 
     $tax01=1.08;
-    $tax02=1.1;
-}*/
+    $tax02=1.1; 
 
 function taxInn($price,$tax01){
     return $price*$tax01;
@@ -87,4 +86,79 @@ function taxIn($price,$tax02){
     return $price*$tax02;
 }
 echo  "カバンは " .taxIn (2000,$tax02). "円です。<br>";
-echo  "コートは " .taxIn (25600,$tax02). "円です。<br>";
+echo  "コートは " .taxIn (25600,$tax02). "円です。<br>";*/
+
+/*function taxInn($price,$tax){
+    $zeikomi = $price*$tax;
+    for( $i = 0; $i < count($items); $i++ ){
+    if($items[$i]['tax']===1);//オレンジは324円です。りんごは270円です。//
+    else ($items[$i]['tax']===2);//カバンは2200円です。コートは28160円です。//
+   
+    return $zeikomi;
+    }
+}
+
+taxInn ($price*$tax);*/ 
+
+function calculateTax($price, $tax) {
+    if ($tax == 1) {
+        return $price * 1.08; // 8% tax
+    } elseif ($tax == 2) {
+        return $price * 1.10; // 10% tax
+    } else {
+        return $price; // if no valid tax provided
+    }
+}
+
+foreach ($items as $item) {
+    $finalPrice = calculateTax($item['price'], $item['tax']);
+    echo $item['name'] . "は" . round($finalPrice) . "円です。 <br>";
+}
+
+/*function Taxiin($tax01, $tax02) {
+    if ($tax01 = 1.08) {
+        return $price * $tax01; // 8% tax
+    } elseif ($tax02 = 1.1) {
+        return $price * $tax02; // 10% tax
+    }
+}
+
+foreach ($items as $item) {
+    $finalPrice = Taxiin($price,$tax01, $tax02);
+    echo $item['name'] . 'は' . round($finalPrice) . '円です。' . PHP_EOL;
+}*/
+
+function taxiin($price, $tax, $tax01, $tax02) {
+    if ($tax == 1) {
+        return $price * $tax01; // 8% tax
+    } elseif ($tax == 2) {
+        return $price * $tax02; // 10% tax
+    }
+}
+
+foreach ($items as $item) {
+    $finalPrice = taxiin($item['price'], $item['tax'], $tax01, $tax02);
+    echo $item['name'] . "は" . round($finalPrice) . "円です。<br>";
+}
+
+
+function calc($price,$tax) {
+    if ($tax == 1) {
+    return $price * 1.08;
+    }
+    elseif ($tax == 2) {
+    return $price * 1.1;
+    }
+}
+foreach ($items as $item) {
+    $finalPrice = calc($item['price'], $item['tax']);
+    echo $item['name'] . "は" . round($finalPrice) . "円です。<br>";
+}
+
+
+
+
+
+
+
+    
