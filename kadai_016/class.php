1ad45d3bc56649b82d43d28d6fe567b6e642aff0
Watanabe-tesuto <nekomo2015@gmail.com>
2024-12-25 00:10:05 +0900

    課題16を再アップロード

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -1,60 +1,45 @@
 <?php
-
-// Foodクラスの定義
 class Food {
-    public $name;
-    public $price;
+    private $name;
+    private $price;
 
-    // コンストラクタ
     public function __construct($name, $price) {
         $this->name = $name;
         $this->price = $price;
     }
 
-    // priceプロパティの値を出力するメソッド
     public function show_price() {
-        echo "Price: {$this->price}円\n";
+        echo $this->price . PHP_EOL;
     }
 }
 
-// Animalクラスの定義
 class Animal {
-    public $name;
-    public $height;
-    public $weight;
+    private $name;
+    private $height;
+    private $weight;
 
-    // コンストラクタ
     public function __construct($name, $height, $weight) {
         $this->name = $name;
         $this->height = $height;
         $this->weight = $weight;
     }
 
-    // heightプロパティの値を出力するメソッド
     public function show_height() {
-        echo "Height: {$this->height} cm\n";
+        echo $this->height . PHP_EOL;
     }
 }
 
-// Foodクラスのインスタンスを生成
-$apple = new Food("Apple", 150);
-$banana = new Food("Banana", 120);
+// Foodクラスのインスタンスを作成
+$food = new Food("potato", 250);
 
-// Animalクラスのインスタンスを生成
-$elephant = new Animal("Elephant", 300, 5000);
-$giraffe = new Animal("Giraffe", 500, 1200);
+// Animalクラスのインスタンスを作成
+$animal = new Animal("dog", 60, 5000);
 
-// インスタンスの詳細を出力
-print_r($apple);
-$apple->show_price();
+// インスタンスの内容をprint_rで出力
+print_r($food);
+print_r($animal);
 
-print_r($banana);
-$banana->show_price();
-
-print_r($elephant);
-$elephant->show_height();
-
-print_r($giraffe);
-$giraffe->show_height();
-
+// show_priceとshow_heightメソッドの呼び出し
+$food->show_price();
+$animal->show_height();
 ?>
